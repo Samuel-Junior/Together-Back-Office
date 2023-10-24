8ecfc92976e8729cae1fff64bcba2c873039d122
Samuel-Jr Ndolou <samuel-jr@MacBook-Pro-de-Samuel-Jr.local>
2023-10-24 12:27:12 +0200

    fin

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -5,10 +5,8 @@ import { Injectable } from "@angular/core";
 @Injectable({
     providedIn: 'root'
   })
-
   export class UsersService {
     
     private BACK_URL = 'http://localhost:3000';
     constructor(private http : HttpClient) { }
-
   }
